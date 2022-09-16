af27aa87e38a988c40583ffb24dfd92a39abb2f6
Alex P <brown.e@ya.ru>
2022-09-16 23:19:47 +0300

    first commit

---
+++ src/main/java/com/dreamteam/towtruckalert/TowTruckAlertApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.towtruckalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowTruckAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TowTruckAlertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamteam/towtruckalert/TowTruckAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.towtruckalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TowTruckAlertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
